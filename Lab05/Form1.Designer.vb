32e7fc15e4f231a4fbd24c2201b1e7e82d31a435
NielCreary <51706025+NielCreary@users.noreply.github.com>
2019-07-27 00:13:33 -0400

    Initial commit

---
+++ Lab05/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.mnuMain = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.txtText = New System.Windows.Forms.RichTextBox()
+        Me.TextEditorToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.mnuMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuMain
+        '
+        Me.mnuMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.mnuMain.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMain.Name = "mnuMain"
+        Me.mnuMain.Size = New System.Drawing.Size(589, 24)
+        Me.mnuMain.TabIndex = 0
+        Me.mnuMain.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem1, Me.SaveAsToolStripMenuItem2, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewToolStripMenuItem.Text = "&New           Ctrl+N"
+        Me.NewToolStripMenuItem.ToolTipText = "Create new file"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "&Open         Ctrl+O"
+        Me.OpenToolStripMenuItem.ToolTipText = "Open an existing file"
+        '
+        'SaveToolStripMenuItem1
+        '
+        Me.SaveToolStripMenuItem1.Name = "SaveToolStripMenuItem1"
+        Me.SaveToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.SaveToolStripMenuItem1.Text = "&Save           Ctrl+S"
+        Me.SaveToolStripMenuItem1.ToolTipText = "Save current file"
+        '
+        'SaveAsToolStripMenuItem2
+        '
+        Me.SaveAsToolStripMenuItem2.Name = "SaveAsToolStripMenuItem2"
+        Me.SaveAsToolStripMenuItem2.Size = New System.Drawing.Size(180, 22)
+        Me.SaveAsToolStripMenuItem2.Text = "Save &As"
+        Me.SaveAsToolStripMenuItem2.ToolTipText = "Save current file and choose name and location"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        Me.ExitToolStripMenuItem.ToolTipText = "Exit the editor"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem, Me.CutToolStripMenuItem, Me.PasteToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
+        Me.CopyToolStripMenuItem.Text = "&Copy         Ctrl+C"
+        Me.CopyToolStripMenuItem.ToolTipText = "Copies selected words"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
+        Me.CutToolStripMenuItem.Text = "Cu&t            Ctrl+X"
+        Me.CutToolStripMenuItem.ToolTipText = "copies selected words while removing them"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
+        Me.PasteToolStripMenuItem.Text = "&Paste         Ctrl+V"
+        Me.PasteToolStripMenuItem.ToolTipText = "Pastes previous copied words"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        Me.AboutToolStripMenuItem.ToolTipText = "Learn more about program"
+        '
+        'txtText
+        '
+        Me.txtText.AcceptsTab = True
+        Me.txtText.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtText.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtText.Location = New System.Drawing.Point(0, 27)
+        Me.txtText.Name = "txtText"
+        Me.txtText.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.txtText.Size = New System.Drawing.Size(589, 408)
+        Me.txtText.TabIndex = 1
+        Me.txtText.Text = ""
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.Filter = "Text Files|*.txt|RTF Files|*.rtf"
+        Me.OpenFileDialog.Title = "Open File"
+        '
+        'SaveFileDialog
+        '
+        Me.SaveFileDialog.Filter = "Text Files|*.txt|RTF Files|*.rtf"
+        Me.SaveFileDialog.Title = "Save File"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(589, 435)
+        Me.Controls.Add(Me.txtText)
+        Me.Controls.Add(Me.mnuMain)
+        Me.MainMenuStrip = Me.mnuMain
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Text Editor"
+        Me.mnuMain.ResumeLayout(False)
+        Me.mnuMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnuMain As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents txtText As RichTextBox
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextEditorToolTip As ToolTip
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+End Class
---
+++ Lab05/Form1.vb
@@ -0,0 +1,140 @@
+﻿Option Strict On
+Imports System.IO
+''' <summary>
+''' Name:       Niel Creary
+''' 
+''' Date:       07/26/2019
+''' 
+''' Assignment: Lab 05
+''' 
+''' Purpose:    Create a simple text editor to save and open files.
+''' 
+''' </summary>
+Public Class frmMain
+    Private Sub EditToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditToolStripMenuItem.Click
+
+    End Sub
+    ''' <summary>
+    ''' Cut selected text
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        txtText.Cut()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+
+    End Sub
+    ''' <summary>
+    ''' see more about text editor
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        About()
+    End Sub
+    ''' <summary>
+    ''' copy selected text
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        txtText.Copy()
+    End Sub
+    ''' <summary>
+    ''' Paste copied text
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        txtText.Paste()
+    End Sub
+    ''' <summary>
+    ''' Message for "About"
+    ''' </summary>
+    Private Sub About()
+        MessageBox.Show("NETD-2202" & vbNewLine & vbNewLine & "Lab #5" & vbNewLine & vbNewLine & "N. Creary", "About", MessageBoxButtons.OK)
+    End Sub
+    
+    Private Sub NewNToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        NewFile()
+        Me.Text = "Text Editor " & SaveFileDialog.FileName
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Me.Text += " Select a file to open"
+        Open()
+        Me.Text = "Text Editor " & OpenFileDialog.FileName
+    End Sub
+
+    Private Sub FileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FileToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem1.Click
+        Me.Text += " Save"
+        Save()
+        Me.Text = "Text Editor " & SaveFileDialog.FileName
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem2.Click
+        Me.Text += " Save As"
+        SaveAs()
+        Me.Text = "Text Editor " & SaveFileDialog.FileName
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+    ''' <summary>
+    ''' Clears text
+    ''' </summary>
+    Private Sub NewFile()
+        txtText.Clear()
+    End Sub
+    ''' <summary>
+    ''' Opens "OpenFileDialog" to find a file to open
+    ''' </summary>
+    Private Sub Open()
+        If OpenFileDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                Dim reader As New StreamReader(OpenFileDialog.FileName)
+                txtText.Text = reader.ReadToEnd
+                reader.Close()
+            Catch ex As Exception
+                Throw New ApplicationException(ex.ToString)
+            End Try
+        End If
+
+    End Sub
+    ''' <summary>
+    ''' Saves current file
+    ''' </summary>
+    Private Sub Save()
+        If (File.Exists(OpenFileDialog.FileName)) Then
+            My.Computer.FileSystem.WriteAllText(OpenFileDialog.FileName, txtText.Text, True)
+        ElseIf (File.Exists(SaveFileDialog.FileName)) Then
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog.FileName, txtText.Text, True)
+        Else
+            SaveAs()
+        End If
+
+    End Sub
+    ''' <summary>
+    ''' Saves file and allows to change name or location
+    ''' </summary>
+    Private Sub SaveAs()
+        SaveFileDialog.Filter = "TXT Files (*.txt*)|*.txt"
+
+        If SaveFileDialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog.FileName, txtText.Text, False)
+        End If
+
+    End Sub
+
+End Class
